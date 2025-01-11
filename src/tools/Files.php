05c7c82bfaac9efc08e45d273f78575559fa8ffc
edujardin <manu7772@gmail.com>
2025-01-11 13:24:29 +0100

    added Files tool

---
+++ src/tools/Files.php
@@ -0,0 +1,52 @@
+<?php
+namespace Aequation\ToolsBundle\tools;
+
+// Symfony
+use Symfony\Component\Yaml\Yaml;
+// PHP
+use SplFileInfo;
+
+class Files 
+{
+
+    public function __toString(): string
+    {
+        return Objects::getShortname(static::class, false);
+    }
+
+    /**
+     * Add $path to $base directory
+     * Remove final /
+     * Replace multiple consecutive / by only one
+     * @param string $base
+     * @param string|null $path
+     * @return string
+     */
+    public static function addPath(
+        string $base,
+        ?string $path = null
+    ): string
+    {
+        $path = empty($path)
+            ? $base
+            : preg_replace('#\\/*$#', '', $base).preg_replace(['#^\\/*#', '#\\/*$#'], ['/',''], $path);
+        return preg_replace('#\\/{2,}#', '/', $path);
+    }
+
+
+    /** YAML files */
+
+    /**
+     * Read a YAML file and return data
+     * @param string|SplFileInfo $file
+     * @return array|null
+     */
+    public function readYamlFile(
+        string|SplFileInfo $file
+    ): array|null
+    {
+        $filepath = $file instanceof SplFileInfo ? $file->getRealPath() : $file;
+        return Yaml::parse(file_get_contents($filepath));
+    }
+
+}
